7a4c0a38d11e49b8a5286abbab72e2740f476918
wrkzg <thetoolfull@gmail.com>
2022-07-20 17:59:02 +0300

    contract file formatting

--- ethereum/contracts/CryptoBook.sol
+++ ethereum/contracts/CryptoBook.sol
@@ -1,53 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ContactFactory{
+contract ContactFactory {
     mapping(address => address) public ownerToContact;
 
-    modifier onlyNewRecords(){
-        require(ownerToContact[msg.sender] == address(0),"You already leave your contact!");
+    modifier onlyNewRecords() {
+        require(
+            ownerToContact[msg.sender] == address(0),
+            "You already leave your contact!"
+        );
         _;
     }
 
-    function createContact(string memory _telegram, string memory _discord) onlyNewRecords public {
-        
-        ownerToContact[msg.sender] = address(new Contact(msg.sender, _telegram, _discord ));
+    function createContact(string memory _telegram, string memory _discord)
+        public
+        onlyNewRecords
+    {
+        ownerToContact[msg.sender] = address(
+            new Contact(msg.sender, _telegram, _discord)
+        );
     }
 
-    function createContact(string memory _telegram) onlyNewRecords public {
-        
-        ownerToContact[msg.sender] = address(new Contact(msg.sender, _telegram, "" ));
+    function createContact(string memory _telegram) public onlyNewRecords {
+        ownerToContact[msg.sender] = address(
+            new Contact(msg.sender, _telegram, "")
+        );
     }
 }
 
-
-contract Contact{
+contract Contact {
     address public owner;
     string public telegram;
     string public discord;
     string public desc;
 
-    constructor (address _owner, string memory _telegram, string memory _discord){
+    constructor(
+        address _owner,
+        string memory _telegram,
+        string memory _discord
+    ) {
         owner = _owner;
         telegram = _telegram;
-        discord = _discord;   
-    }  
+        discord = _discord;
+    }
 
-    modifier onlyOwner(){
+    modifier onlyOwner() {
         require(owner == msg.sender, "you're not owner!!!");
         _;
     }
 
-    function setTelegram (string memory _telegram) public onlyOwner {
-        
+    function setTelegram(string memory _telegram) public onlyOwner {
         telegram = _telegram;
     }
 
-    function setDiscord (string memory _discord) public onlyOwner {
+    function setDiscord(string memory _discord) public onlyOwner {
         discord = _discord;
     }
 
-    function setDesc (string memory _desc) public onlyOwner {
+    function setDesc(string memory _desc) public onlyOwner {
         desc = _desc;
     }
 }
